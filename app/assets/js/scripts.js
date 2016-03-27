71e69653dc1708c74f0ba8b52ca1988e1811d8a9
Rowan Weismiller <rowan.weismiller@gmail.com>
2016-03-26 23:39:49 -0700

    Writes basic JS skeleton

---
+++ app/assets/js/scripts.js
@@ -0,0 +1,35 @@
+// Notes app!
+// ---
+// display selected note
+// make note
+// update note
+// delete note
+// get list of notes (and automatically select the first one)
+// save to localStorage
+
+var Notes = (function() {
+
+    var notes = [];
+
+    saveNotes: function() {
+
+    }
+
+    return {
+        addNote: function(title, date, body) {
+
+        },
+        updateNote: function(title, date, body) {
+
+        },
+        deleteNote: function(id) {
+
+        },
+        getNote: function(id) {
+
+        },
+        createNoteList: function() {
+
+        }
+    }
+}());
