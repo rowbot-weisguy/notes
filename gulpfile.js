da1b58de4c2a765ce8277696f047db8e5c6ac52e
Rowan Weismiller <rowan.weismiller@gmail.com>
2016-03-26 17:34:26 -0700

    Got the scaffold working all properly

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,136 @@
+// Lots borrowed from http://www.mikestreety.co.uk/blog/an-advanced-gulpjs-file
+// Path configs first
+
+var basePaths = {
+    src: './app/assets/',
+    dest: './public/assets/'
+};
+var paths = {
+    html: {
+        src: './app/',
+        dest: './public/',
+    },
+    images: {
+        src: basePaths.src + 'images/',
+        dest: basePaths.dest + 'images/min/'
+    },
+    scripts: {
+        src: basePaths.src + 'js/',
+        dest: basePaths.dest + 'js/min/'
+    },
+    styles: {
+        src: basePaths.src + 'sass/',
+        dest: basePaths.dest + 'css/min/'
+    }
+};
+
+var appFiles = {
+    html: paths.html.src + '**/*.html',
+    styles: paths.styles.src + '**/*.scss',
+    scripts: [paths.scripts.src + 'scripts.js']
+};
+
+var vendorFiles = {
+    styles: '',
+    scripts: ''
+};
+
+// Gulp stuff begin
+
 var gulp = require('gulp');
-var sass = require('gulp-sass');
-var browserSync = require('browser-sync');
-var reload = browserSync.reload;
 
-gulp.task('styles', function() {
-    gulp.src('app/source/sass/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('app/styles'))
-        .pipe(reload({ stream:true }));
+var es = require('event-stream');
+var gutil = require('gulp-util');
+var scsslint = require('gulp-scss-lint');
+var browserSync = require('browser-sync').create();
+
+var plugins = require("gulp-load-plugins")({
+    pattern: ['gulp-*', 'gulp.*'],
+    replaceString: /\bgulp[\-.]/
 });
 
-gulp.task('watch', function() {
-    gulp.watch('app/source/sass/**/*.scss', ['styles']);
+// Allows gulp --dev to be run for a more verbose output
+var isProduction = true;
+var sassStyle = 'compressed';
+var sourceMap = false;
+
+if(gutil.env.dev === true) {
+    sassStyle = 'expanded';
+    sourceMap = true;
+    isProduction = false;
+}
+
+var changeEvent = function(evt) {
+    gutil.log('File', gutil.colors.cyan(evt.path.replace(new RegExp('/.*(?=/' + basePaths.src + ')/'), '')), 'was', gutil.colors.magenta(evt.type));
+};
+
+gulp.task('html', function() {
+    return gulp.src(appFiles.html)
+        .pipe(gulp.dest(paths.html.dest));
 });
 
-gulp.task('serve', ['styles'], function() {
-    browserSync({
-        server: {
-            baseDir: 'app'
-        }
+gulp.task('css', function(){
+    var sassFiles = gulp.src(appFiles.styles)
+    .pipe(plugins.rubySass({
+        style: sassStyle, sourcemap: sourceMap, precision: 2
+    }))
+    .on('error', function(err){
+        new gutil.PluginError('CSS', err, {showStack: true});
     });
 
-    gulp.watch('app/source/sass/**/*.scss', ['styles']);
+    return es.concat(gulp.src(vendorFiles.styles), sassFiles)
+        .pipe(plugins.concat('style.min.css'))
+        .pipe(plugins.autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+        .pipe(isProduction ? plugins.combineMediaQueries({
+            log: true
+        }) : gutil.noop())
+        .pipe(isProduction ? plugins.cssmin() : gutil.noop())
+        .pipe(plugins.size())
+        .pipe(gulp.dest(paths.styles.dest))
+        .pipe(browserSync.stream());
 });
+
+gulp.task('scripts', function(){
+    gulp.src(vendorFiles.scripts.concat(appFiles.scripts))
+        .pipe(plugins.concat('app.js'))
+        .pipe(gulp.dest(paths.scripts.dest))
+        .pipe(isProduction ? plugins.uglify() : gutil.noop())
+        .pipe(plugins.size())
+        .pipe(gulp.dest(paths.scripts.dest));
+
+});
+
+gulp.task('watch', ['css', 'scripts'], function(){
+    gulp.watch(appFiles.styles, ['css']).on('change', function(evt) {
+        changeEvent(evt);
+    });
+    gulp.watch(paths.scripts.src + '*.js', ['scripts']).on('change', function(evt) {
+        changeEvent(evt);
+    });
+});
+
+
+gulp.task('scss-lint', function() {
+  return gulp.src(appFiles.styles)
+    .pipe(scsslint());
+});
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['css'], function() {
+
+    browserSync.init({
+        server: "./public"
+    });
+
+    gulp.watch(appFiles.styles, ['build']).on('change', function(evt) {
+        changeEvent(evt);
+    });
+    gulp.watch(appFiles.html, ['build']).on('change', function(evt) {
+        changeEvent(evt);
+    });
+    gulp.watch(appFiles.html).on('change', browserSync.reload);
+});
+
+
+gulp.task('default', ['html', 'css', 'scripts', 'serve']);
+gulp.task('build', ['html', 'css', 'scripts']);
