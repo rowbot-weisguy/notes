fd4e450c48a7ff0c82c7df4f7658c83cae977459
Rowan Weismiller <rowan.weismiller@gmail.com>
2016-03-28 16:14:24 -0700

    Removes more unnecessary gulp packages

--- gulpfile.js
+++ gulpfile.js
@@ -86,10 +86,6 @@ gulp.task('css', function(){
     return es.concat(gulp.src(vendorFiles.styles), sassFiles)
         .pipe(plugins.concat('style.min.css'))
         .pipe(plugins.autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-        .pipe(isProduction ? plugins.combineMediaQueries({
-            log: true
-        }) : gutil.noop())
-        .pipe(isProduction ? plugins.cssmin() : gutil.noop())
         .pipe(plugins.size())
         .pipe(gulp.dest(paths.styles.dest))
         .pipe(browserSync.stream());
@@ -103,8 +99,6 @@ gulp.task('icons', function() {
 gulp.task('scripts', function(){
     gulp.src(vendorFiles.scripts.concat(appFiles.scripts))
         .pipe(plugins.concat('app.js'))
-        .pipe(gulp.dest(paths.scripts.dest))
-        .pipe(isProduction ? plugins.uglify() : gutil.noop())
         .pipe(plugins.size())
         .pipe(gulp.dest(paths.scripts.dest));
 
